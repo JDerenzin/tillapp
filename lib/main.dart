21f4a9b642aaeccb939931dff84a323cd186badf
jderenzin <valhallaproject70@gmail.com>
2026-09-12 12:19:24 -0500

    Importamos dependencias: supabase_flutter y shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const Tillapp());
